c26e54999c2218dc410c1eb05ffd5c017e20740c
Pamela Hicks <pamelasusanhicks@gmail.com>
2018-10-09 10:46:24 -0400

    Babel polyfill

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['@babel/polyfill', './src/index.js'],
   plugins: [
     new webpack.EnvironmentPlugin([
       'NODE_ENV',
